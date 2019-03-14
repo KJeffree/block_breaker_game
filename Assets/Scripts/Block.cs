636eeb587578ca38c6f2c066bd58b412ee9c4ab9
KJeffree <k.jeffree@gmail.com>
2019-03-14 11:04:51 +0000

    can count blocks, remove blocks, and move on to next level

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] AudioClip breakSound;
+
+    //cahced reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+   private void OnCollisionEnter2D(Collision2D collision)
+   {
+       AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+       Destroy(gameObject);
+       level.RemoveBlock();
+   }
+
+}
