c6b3a72734c2ea9d4e3ee8a9c08e6f779acab6b6
KJeffree <k.jeffree@gmail.com>
2019-03-18 11:56:14 +0000

    added max hits and hit counter to blocks, and if statement to check if the block has been hit the max number of times

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    // config params
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] int maxHits = 3;
 
     //cahced reference
     Level level;
     GameSession gameStatus;
+
+    //state variables
+    [SerializeField] int timesHit; //only serizlised for debug purposes
+
 
     private void Start()
     {
@@ -29,13 +35,22 @@ public class Block : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
    {
        if (tag == "Breakable")
-       {
-           DestroyBlock();
-           TriggerSparklesVFX();
-       }
-   }
+        {
+            HandleHit();
+        }
+    }
 
-   private void DestroyBlock()
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+            TriggerSparklesVFX();
+        }
+    }
+
+    private void DestroyBlock()
    {
        Destroy(gameObject);
        level.RemoveBlock();
